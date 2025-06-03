c7dda55489356d7fd80dbb648d1b2d3fbccad62a
Harsh <harshghongade21@gmail.com>
2025-06-03 19:56:10 +0530

    Updated till MLflow autolog

---
+++ test.py
@@ -0,0 +1,7 @@
+import mlflow
+print("printing tracking uri scheme below :")
+print(mlflow.get_tracking_uri())
+print("\n")
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+print(mlflow.get_tracking_uri())
